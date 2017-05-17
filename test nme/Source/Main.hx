0a653ff5079e88e8cdecdf5fb3d286ddad39160d
Jeff Ward <jeff.ward@wootmath.com>
2017-05-17 09:04:47 -0600

    Added 'test nme', working on linux64, haxe 3.4.2, hxcpp 3.4.90, nme 5.7.1

---
+++ test nme/Source/Main.hx
@@ -0,0 +1,26 @@
+import nme.display.Bitmap;
+import nme.display.Sprite;
+import nme.Assets;
+import nme.Lib;
+
+
+class Main extends Sprite {
+	
+	
+	public function new () {
+
+#if debug
+		new debugger.HaxeRemote(true, "localhost");
+#end
+		super ();
+		
+		var bitmap = new Bitmap (Assets.getBitmapData ("assets/nme.png"));
+		addChild (bitmap);
+		
+		bitmap.x = (Lib.current.stage.stageWidth - bitmap.width) / 2;
+		bitmap.y = (Lib.current.stage.stageHeight - bitmap.height) / 2;
+		
+	}
+	
+	
+}
